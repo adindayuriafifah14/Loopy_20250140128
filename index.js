88745979c9c189eb3786ae95b472ab058a486bc6
adindayuriafifah14 <adindaafifah14@gmail.com>
2026-06-09 18:38:13 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var index =
+[
+    [ "Introduction", "index.html#Introduction", null ],
+    [ "Operation", "index.html#Operation", null ],
+    [ "Penggunaan", "index.html#Cara", null ]
+];
--- search/searchdata.js
+++ search/searchdata.js
@@ -1,18 +1,27 @@
 var indexSectionsWithContent =
 {
-  0: "c",
-  1: "c"
+  0: "cdimpqr",
+  1: "q",
+  2: "c",
+  3: "dimqr",
+  4: "cdpq"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "files"
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Files"
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Pages"
 };
 
